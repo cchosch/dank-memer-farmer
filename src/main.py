faafe1fdd79c59fff195767a9e3652f5833ec698
MeTA2X <colinhoscheit@gmail.com>
2022-02-02 23:25:05 -0500

    going to bed

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 import requests
+import json
+
+def read_config():
+    theconfigfile = open("config.json")
+    theconfig = json.load(theocnfigfile)
+    theconfigfile.close()
+    return theconfig["token"], theconfig["channelid"]
+
+TOKEN, CHANNEL_ID = read_config()
+
+def update_glob_vars():
+    TOKEN, CHANNEL_ID = read_config()
 
 def main():
-    pass
-
+    auth_header = {"Authorization":f"{TOKEN}"}
+    while True:
+        pass
+    
 if __name__ == "__main__":
     main()
